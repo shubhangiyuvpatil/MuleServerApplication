51870c0122c5e0398381621a2673af9ddf1d6d54
shubhangiyuvpatil <98641657+shubhangiyuvpatil@users.noreply.github.com>
2022-01-29 17:00:51 +0530

    first commit

---
+++ src/main/java/com/example/MuleServerApplication/MuleServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.MuleServerApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MuleServerApplication/MuleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MuleServerApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
